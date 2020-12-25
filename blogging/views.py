681aabb6de3e8876f659e1433d6f82d0ca3fc750
stellie <kims82@gmail.com>
2020-12-25 13:41:59 -0800

    status post lesson 8 additions of Django apps

--- blogging/views.py
+++ blogging/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
+from django.utils import timezone
+from django import forms
 from blogging.models import Post
+from blogging.forms import PostForm
 
 
 def list_view(request):
@@ -19,3 +22,17 @@ def detail_view(request, post_id):
         raise Http404
     context = {'post': post}
     return render(request, 'blogging/detail.html', context)
+
+
+def create_post(request):
+    print('model form request test')
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            model_instance = form.save(commit=False)
+            model_instance.timestamp = timezone.now()
+            model_instance.save()
+            return redirect('/')
+    else:
+        form = PostForm()
+        return render(request, 'blogging/post.html', {'form': form})
